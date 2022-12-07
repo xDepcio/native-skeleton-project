d69e844afc8912284a3fb5c9bd367914bbbd355e
Aleksander <olek.drwal@gmail.com>
2022-12-07 16:38:26 +0100

    Working skeleton

--- src/ScanComp.js
+++ src/ScanComp.js
@@ -1,21 +1,116 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import Sound from "react-native-sound"
+import NfcManager, {NfcEvents} from "react-native-nfc-manager";
+
+Sound.setCategory('Playback')
+
+let ding = new Sound('bad_omens_like_a_villain.mp3', Sound.MAIN_BUNDLE, (error) => {
+    if (error) {
+        console.log('failed to load sound', error)
+        return
+    }
+    console.log('duration is seconds: ' + ding.getDuration() + 'number of channels: ' + ding.getNumberOfChannels())
+})
+
+function setSoundFromTagId(id) {
+    const idsToSound = {
+        'D3CD5A1E': 'bad_omens_like_a_villain.mp3',
+        'D2F8EC19': 'bfmv_10_years_today.mp3'
+    }
+
+    let sound = idsToSound[id]
+
+    ding = new Sound(sound, Sound.MAIN_BUNDLE, (error) => {
+        if (error) {
+            console.log('failed to load sound', error)
+            return
+        }
+        console.log('duration is seconds: ' + ding.getDuration() + 'number of channels: ' + ding.getNumberOfChannels())
+    })
+}
+
 
 function ScanComp() {
-    const [data, setData] = useState('brak')
+    const [visible, setVisible] = useState(false)
+
+    useEffect(() => {
+        NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag) => {
+            if(Platform.OS === 'android') {
+                // androidPromptRef.current.setHintText(`${count}...`)
+            }
+            if(1 <= 0) {
+                NfcManager.unregisterTagEvent().catch(() => 0)
+                // setDuration(new Date().getTime() - start.getTime())
+
+                if(Platform.OS === 'android') {
+                    // androidPromptRef.current.setVisible(false)
+                }
+            }
+            console.warn('tag found', tag)
+            setVisible(false)
+            NfcManager.unregisterTagEvent()
+            setSoundFromTagId(tag.id)
+            playSound()
+        })
+
+        return () => {
+            NfcManager.setEventListener(NfcEvents.DiscoverTag, null)
+        }
+    }, [])
+
+
+
+    function playSound() {
+        ding.play(success => {
+            if (success) {
+              console.log('successfully finished playing');
+            } else {
+              console.log('playback failed due to audio decoding errors');
+            }
+        })
+    }
+
+    function stopPlaySound() {
+        ding.pause()
+    }
+
+    function restartPlaySound() {
+        ding.stop()
+        ding.play()
+    }
+
+    async function scanTag() {
+        setVisible(true)
+        await NfcManager.registerTagEvent()
+        if(Platform.OS === 'android') {
+            // androidPromptRef.current.setVisible(true)
+            // console.warn('t')
+        }
+    }
 
     return (
+        <>
         <View style={styles.wrapper}>
-            <Text>NFC Game</Text>
-            <TouchableOpacity style={styles.btn} onPress={async () => {
-                const response = await fetch('https://api.namefake.com/')
-                const result = await response.json()
-                setData(result.name)
-            }}>
+            <TouchableOpacity style={styles.btn} onPress={playSound}>
                 <Text>START</Text>
             </TouchableOpacity>
-            <Text>{data}</Text>
+            <TouchableOpacity style={styles.btn} onPress={stopPlaySound}>
+                <Text>PAUSE</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.btn} onPress={restartPlaySound}>
+                <Text>REPLAY</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.btn} onPress={scanTag} >
+                <Text>SCAN NFC TAG</Text>
+            </TouchableOpacity>
         </View>
+        {visible && (
+            <View style={styles.popup}>
+                <Text style={styles.text}>SCAN TAG</Text>
+            </View>
+        )}
+        </>
     )
 }
 
@@ -31,7 +126,24 @@ const styles = StyleSheet.create({
         borderWidth: 3,
         borderRadius: 9,
         padding: 10,
-        paddingHorizontal: 20,
+        width: 150,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    popup: {
+        position: 'absolute',
+        width: '100%',
+        backgroundColor: 'white',
+        bottom: 0,
+        height: 200,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        color: 'black',
+        fontSize: 20
     }
 })
 
